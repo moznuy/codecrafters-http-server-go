019da5612b3d0b9250c3e1e9c1b3c15f54063f11
Serhii Charykov <laammaar@gmail.com>
2024-03-12 09:46:06 +0200

    Respond with 200

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -14,9 +15,32 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	client, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	var data [4096]byte
+	//for {
+	read, err := client.Read(data[:])
+	s := string(data[:read])
+	//fmt.Printf("read: %v %#v\n", read, data[:read])
+	fmt.Printf("%s", s)
+	if err != nil {
+		if err == io.EOF {
+			os.Exit(1) // break
+		}
+		fmt.Println("Error reading connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = client.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	//}
+	fmt.Println("client disconnected")
 }
